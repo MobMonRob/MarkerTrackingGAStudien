35eb459ea251f3d1dd12b56eca3c3c51964990bb
Moritz Hasenschwandtner <154317849+moritz-ha@users.noreply.github.com>
2026-03-02 19:32:55 +0100

    implemented least squares method to approximate intersect between n rays

---
+++ python-prototype-LA/algorithms/lsp.py
@@ -0,0 +1,34 @@
+from typing import Tuple, List
+import numpy as np
+from jaxtyping import Float
+
+def approximate_intersect(points: List[Float[np.ndarray, "3 1"]], direction_vectors: List[Float[np.ndarray, "3 1"]]):
+    """
+    Compute the point minimising the squared distance between n lines.
+
+    Source: https://math.stackexchange.com/questions/36398/point-closest-to-a-set-four-of-lines-in-3d
+
+    points: List of (3, 1) arrays
+    direction_vectors: List of (3, 1) arrays
+    """
+    
+    if len(points) != len(direction_vectors):
+        raise ValueError("Need to provide exactly as many points p_i as direction vectors d_i")
+
+    M = np.zeros((3, 3)) # M = Sum of all A_i
+    b = np.zeros((3, 1)) # b = Sum of all A_i * p_i
+
+    for i in range(len(points)):
+        p_i = points[i]
+        d_i = direction_vectors[i]
+        d_i = d_i / np.linalg.norm(d_i)
+
+        # A_i = I - d_i*d_i^T
+        A_i = np.eye(3) - d_i @ d_i.T
+        M = M + A_i
+
+        b += A_i @ p_i
+    
+    computed_point_x = np.linalg.solve(M, b)
+
+    return computed_point_x
--- python-prototype-LA/multi_camera_line_dump.py
+++ python-prototype-LA/multi_camera_line_dump.py
@@ -4,14 +4,15 @@ from visualisation.image_plane_grid import plot_image_plane_grid, plot_single_im
 import visualisation.scene as scene
 import matplotlib.pyplot as plt
 from algorithms.lvp import lotfusspunkt_verfahren
+from algorithms.lsp import approximate_intersect
 from geometry.rays import ray_batch_from_to
 import argparse
 
 """
 Script intendet to be run with:
-- xcp = camera-configs/experiment-1.xcp
-- markers =  csvdata/experiment-1/marker-dump-1-marker-all-cams.csv
-- centroids = csvdata/experiment-1/centroid-dump-1-marker-all-cams.csv 
+- xcp = camera-configs/experiment-3.xcp
+- markers =  csvdata/experiment-3/all-cams-masked/marker-dump-1-marker-all-cams.csv
+- centroids = csvdata/experiment-3/all-cams-masked/centroid-dump-1-marker-all-cams.csv 
 """
 
 parser = argparse.ArgumentParser("playground")
@@ -33,11 +34,36 @@ for config in configs:
 
 # visualisation of 3D scene, getting only those cameras such that 4 lines intersect at the point
 fig, ax = scene.create_3d_figure()
-for camera in [cams["3"], cams["6"], cams["4"], cams["5"]]:
-    wcs_points = camera.get_all_wcs_points()
+for camera in cams.values():
     scene.plot_camera(ax, camera)
 
-# Render the dumped points "Musterlösung"
+# Render & print the dumped points "Musterlösung"
 scene.scatter_3d_markers(ax, markers, color="green")
+print("---- API Solution ----")
+for marker in markers:
+    print(marker)
+print("----------------------")
+
+all_ps = []
+all_ds = []
+
+for camera in cams.values():
+    wcs_points = camera.get_all_wcs_points()
+    if np.isnan(np.min(wcs_points[0])):
+        continue
+
+    # Output the y = p + td form of each Line
+    print(f"{camera.user_id}: {np.ndarray.flatten(camera.get_position())} + t * {np.ndarray.flatten(camera.get_position() - wcs_points[0])}")
+
+    all_ps.append(camera.get_position())
+    all_ds.append(camera.get_position() - wcs_points[0])
+
+computed_marker = approximate_intersect(all_ps, all_ds)
+ax.scatter(*computed_marker, color="red", s = 20, marker='x')
+print("---- Calculated Solution ----")
+print(computed_marker)
+print("----------------------")
+
+print(f'Error between computed and actual marker: {np.linalg.norm(computed_marker - markers[0])}')
 
 plt.show()
